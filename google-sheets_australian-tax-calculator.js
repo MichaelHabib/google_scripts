d08135e5937db325175cdd7af6f16be315d9a019
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2018-02-02 18:50:25 +1100

    Update google-sheets_australian-tax-calculator.js

--- google-sheets_australian-tax-calculator.js
+++ google-sheets_australian-tax-calculator.js
@@ -8,7 +8,7 @@ range_start =
 range_end = 
 */
 
-function myFunction(wage) {
+function au_tax(wage) {
   var tax_rates = [
     {"tax_rate": 0,
      "tax_min_payable": 0,
@@ -42,6 +42,62 @@ function myFunction(wage) {
     var rate = rates[i];
     if(wage >= rate.range_start){
       tax = rate.tax_min_payable + ((wage - rate.range_start) * rate.tax_rate);
+      return tax;
+    };
+    
+      
+  }
+  
+}
+/*
+
+Formula Source : https://www.ato.gov.au/Rates/Schedule-1---Statement-of-formulas-for-calculating-amounts-to-be-withheld/
+The formulas comprise linear equations of the form y = ax − b, where:
+
+y is the weekly withholding amount expressed in dollars
+x is the number of whole dollars in the weekly earnings plus 99 cents
+a and b are the values of the coefficients for each set of formulas for each range of weekly earnings (or, in the case of fortnightly, monthly or quarterly earnings, the weekly equivalent of these amounts).
+
+*/
+function au_tax_weekly(weeklyincome) {
+  var scale_1 = [
+    {"max_income": 66,
+     "a": 0.19,
+     "b": 0.19,
+     "dollar_adjustments_to_income": 0.99
+    },
+      {"max_income": 361,
+     "a": 0.2337,
+     "b": 2.9035,
+     "dollar_adjustments_to_income": 0.99
+    },
+        {"max_income": 932,
+     "a": 0.3477,
+     "b": 44.1189,
+     "dollar_adjustments_to_income": 0.99
+    },
+        {"max_income": 1323,
+     "a": 0.3450,
+     "b": 41.6024,
+     "dollar_adjustments_to_income": 0.99
+    },
+        {"max_income": 3111,
+     "a": 0.3900,
+     "b": 101.1408,
+     "dollar_adjustments_to_income": 0.99
+    },
+      {"max_income": 9999999999,
+     "a": 0.4700,
+     "b": 350.0639,
+     "dollar_adjustments_to_income": 0.99
+    }
+    ];
+
+  //return wage;
+  for (var i = 0, tax = 0, scale_item = scale_1; scale_1.length > i ; i++) {
+    var scale_item = scale_1[i];
+    if(weeklyincome > scale_item.max_income){
+      tax = scale_item.a * ((weeklyincome + scale_item.dollar_adjustments_to_income) - scale_item.b);
       return tax;
     };
     
